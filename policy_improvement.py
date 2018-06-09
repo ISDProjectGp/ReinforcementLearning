f6fff2bd58cdf8c7ba57cadb63c14722873c1e53
fung <funghiantie@gmail.com>
2018-06-09 14:14:06 +0800

    fung

--- policy_improvement.py
+++ policy_improvement.py
@@ -2,7 +2,7 @@ import numpy as np
 from random import randint
 
 
-class PolicyEvaluation:
+class PolicyImprovement:
 
     def __init__(self, deterministic_state):
         self.deterministic_state = deterministic_state
@@ -23,6 +23,7 @@ class PolicyEvaluation:
                 print(cumulative_reward_matrix[i][j])
 
     def execute(self):
+        convergence_threshold = 2
         reward_matrix = self.deterministic_state.get_reward_matrix()
         policy_matrix = self.deterministic_state.get_policy_matrix()
         cumulative_reward_matrix = self.deterministic_state.get_cumulative_reward_matrix()
@@ -45,22 +46,22 @@ class PolicyEvaluation:
                     a = a - 1
         self.print_policy(policy_matrix)
         # policy iteration
-        convergence_threshold = 0.3
-        max_diff = 1
+        max_diff = 99999
         discount = 0.6
         # if the absolute difference is bigger than  convergence threshold
         while max_diff > convergence_threshold:
             max_diff = 0
             # run bellmen equation in each state
             for i in range(0, self.deterministic_state.get_n()):
+                original_v = reward_matrix[i, policy_matrix[i]]
                 if policy_matrix[i] != -1:
-                    original_v = cumulative_reward_matrix[i, policy_matrix[i]]
                     if policy_matrix[policy_matrix[i]] == -1:
                         cumulative_reward_matrix[i, policy_matrix[i]] = reward_matrix[i, policy_matrix[i]]
                     else:
                         cumulative_reward_matrix[i, policy_matrix[i]] = reward_matrix[i, policy_matrix[i]] + \
-                                                                        discount * cumulative_reward_matrix[
+                                                                        discount*cumulative_reward_matrix[
                                                                             policy_matrix[i], policy_matrix[
                                                                                 policy_matrix[i]]]
-                    max_diff = self.max(max_diff, abs(original_v - cumulative_reward_matrix[i, policy_matrix[i]]))
+                max_diff = self.max(max_diff, abs(original_v - cumulative_reward_matrix[i, policy_matrix[i]]))
         self.print_cumulative_reward_matrix(cumulative_reward_matrix)
+
